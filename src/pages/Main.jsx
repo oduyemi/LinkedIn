b9bb6cb15496b2106b8c3828515a4c295eba5227
Yemi Oduyemi <yhermii@gmail.com>
2024-09-23 18:34:17 +0100

    responsiveness

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,18 +1,18 @@
 import React from "react";
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, useBreakpointValue } from "@chakra-ui/react";
 import { Sidebar } from "../components/Sidebar";
 import { MainContent } from "../components/MainContent";
 import { RightContent } from "../components/RightContent";
 import { Footer } from "../components/Footer";
 
-
-
 const Main = () => {
+    const flexDirection = useBreakpointValue({ base: "column", md: "row" });
+
     return (
         <>
-            <Flex mt={10}>
-                <Sidebar />
-                <Box flex="1" p={4} mt={10}>
+            <Flex direction={flexDirection} mt={10} mb={10}>
+                {flexDirection === "row" && <Sidebar />}
+                <Box flex="1" p={4} mt={flexDirection === "row" ? 0 : 4}>
                     <MainContent />
                 </Box>
                 <RightContent />
@@ -20,8 +20,6 @@ const Main = () => {
             <Footer />
         </>
     );
-}
-
-
+};
 
 export default Main;
